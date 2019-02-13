74e4cbae9783c0ef8a835d307655196bfe6147a1
anandmg101 <mg.anand@gmail.com>
2019-02-12 21:12:15 -0600

    Update RearViewCamera.py

--- RearViewCamera.py
+++ RearViewCamera.py
@@ -6,7 +6,7 @@ GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(3, GPIO.OUT) 
 
 try:  
-    print "Waiting for falling edge on port 23"  
+    print ("Waiting for falling edge on port 23")
     GPIO.wait_for_edge(2, GPIO.RISING)  
     time.sleep(5)
     if GPIO.input(2):
@@ -21,6 +21,3 @@ try:
             time.sleep(1)
             GPIO.output(3, GPIO.HIGH)
             RVC_set=0
-
-# https://thepihut.com/blogs/raspberry-pi-tutorials/27968772-turning-on-an-led-with-your-raspberry-pis-gpio-pins
-  
